3777b80b2d0b73a8597f9ac939b8562162959275
Pavel Bondarchuk <pavel.bondarchuk@hys-enterprise.com>
2020-07-10 16:43:25 +0300

    SamuraiContext class setup. Samurai domain class setup (whoops..)

---
+++ Samurai/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData";
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
--- Samurai/SamuraiApp.Domain/Samurai.cs
+++ Samurai/SamuraiApp.Domain/Samurai.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace SamuraiApp.Domain
 {
-    class Samurai
+    public class Samurai
     {
+        public Samurai()
+        {
+            Quotes = new List<Quote>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Quotes { get; set; }
+        public Clan Clan { get; set; }
+
     }
 }
