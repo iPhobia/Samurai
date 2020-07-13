4a776fc47c31313980c3464c8ee1293241a940c6
Pavel Bondarchuk <pavel.bondarchuk@hys-enterprise.com>
2020-07-13 16:04:26 +0300

    added logging

--- Samurai/SamuraiApp.Data/SamuraiContext.cs
+++ Samurai/SamuraiApp.Data/SamuraiContext.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SamuraiApp.Domain;
 using System;
+using Microsoft.Extensions.Logging;
 
 namespace SamuraiApp.Data
 {
     public class SamuraiContext : DbContext
     {
+        public static readonly ILoggerFactory ConsoleLoggerFactory
+            = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter((category, level) =>
+                        category == DbLoggerCategory.Database.Command.Name
+                        && level == LogLevel.Information)
+                    .AddConsole();
+            });
+
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
@@ -16,7 +27,9 @@ namespace SamuraiApp.Data
             string connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData";
             Console.WriteLine($"Configuring database\nConnection string: {connectionString}");
 
-            optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder
+                .UseLoggerFactory(ConsoleLoggerFactory)
+                .UseSqlServer(connectionString);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
