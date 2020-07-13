83501fe7116c217995dd5b24c2fd11a3a34509de
Pavel Bondarchuk <pavel.bondarchuk@hys-enterprise.com>
2020-07-13 14:27:59 +0300

    one-to-one relationship between Samurai and Horse classes. Db migration done

--- Samurai/SamuraiApp.Data/SamuraiContext.cs
+++ Samurai/SamuraiApp.Data/SamuraiContext.cs
@@ -9,6 +9,7 @@ namespace SamuraiApp.Data
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
+        public DbSet<Battle> Battles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         { 
@@ -21,6 +22,8 @@ namespace SamuraiApp.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<SamuraiBattle>().HasKey(sb => new { sb.SamuraiId, sb.BattleId });
+            modelBuilder.Entity<Horse>().ToTable("Horses");
+
         }
     }
 }
