c2eb0e5b0c6abf418b03be24e527df8099e113a7
Pavel Bondarchuk <pavel.bondarchuk@hys-enterprise.com>
2020-07-13 17:52:09 +0300

    workaround with EF features

--- Samurai/ConsoleApp1/Program.cs
+++ Samurai/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using SamuraiApp.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Data;
 using SamuraiApp.Domain;
 using System;
 using System.Linq;
@@ -7,28 +8,55 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        private static SamuraiContext context = new SamuraiContext();
+        private static SamuraiContext _context = new SamuraiContext();
         static void Main(string[] args)
         {
             // looks if database is created in current context, if not creates looking at context
-            var isCreated = context.Database.EnsureCreated() ? "database is not created. Creating..." : "database is already created...";
-            Console.WriteLine(isCreated);
+            //var isCreated = _context.Database.EnsureCreated() ? "database is not created. Creating..." : "database is already created...";
+            //Console.WriteLine(isCreated);
 
-            GetSamurais("Before add:");
-            AddSamurais();
-            GetSamurais("After add:");
+            //GetSamurais("Before add:");
+            //AddSamurai();
+            //InsertMultipleSamurais();
+            //InsertDifferentTables();
+            //GetSamurais("After add:");
+            QueryFilters();
         }
 
-        private static void AddSamurais()
+        private static void QueryFilters()
+        {
+            var name = "Pasha";
+            var samurais = _context.Samurais.Where(s => EF.Functions.Like(s.Name, "P%")).ToList();
+        }
+
+        private static void InsertDifferentTables()
+        {
+            var samurai = new Samurai { Name = "Idiot" };
+            var clan = new Clan { ClanName = "Imperial" };
+            _context.AddRange(samurai, clan);
+            _context.SaveChanges();
+        }
+
+        private static void InsertMultipleSamurais()
+        {
+            var samurai = new Samurai { Name = "Pasha" };
+            var samurai2 = new Samurai { Name = "Denis" };
+            var samurai3 = new Samurai { Name = "Vasya" };
+            var samurai4 = new Samurai { Name = "Kolya" };
+            _context.Samurais.AddRange(samurai, samurai2, samurai3, samurai4);
+            _context.SaveChanges();
+        }
+
+        private static void AddSamurai()
         {
             var samurai = new Samurai { Name = "Lyoha" };
-            context.Samurais.Add(samurai);
-            context.SaveChanges();
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
         }
 
         private static void GetSamurais(string text)
         {
-            var samurais = context.Samurais.ToList();
+            var samurais = _context.Samurais.ToList();
             Console.WriteLine($"{text}\n\tSamurais count is {samurais.Count()}");
             foreach (var samurai in samurais)
             {
